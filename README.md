b04af87fb326f6383922b6f3bfa2b97c637ce936
Philip <phill@omega323.com>
2012-12-03 00:35:30 +0100

    Modified extract-files.sh to fit a little bit more for codina. Additionally, I have removed one SED call in extract-files.sh and created another script to do the job and save me fucking time copying libraries on breaks... create-vendor-blobs.php. ENJOY! (Yes I will use PHP, shut up)

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ Samsung Galaxy Ace II (GT-I8160) Device
 =============
 
 Those are device configuration files for Galaxy Ace II.
+
+Warning!
+=============
+If you wish to use this, make sure you have PHP installed on your system.
+At least **PHP 5.1**.
+
+On Debian based distributions (such us Ubuntu), you can install it with this:
+***apt-get install php5 php5-cli***
+
+Why? Because it will be needed in order to set-up vendor blobs...
+But why **PHP**?! Because I said so.
---
+++ create-vendor-blobs.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * copyright (c) 2012 by bagnz0r
+ * http://github.com/bagnz0r
+ *
+ * this piece of code is released under the new bsd license.
+ */
+
+$vendor_dir = '../../../vendor/samsung';
+$blob_fhandle = fopen($vendor_dir . '/u8500-vendor-blobs.mk', 'w');
+
+scrap_folders($vendor_dir . '/proprietary', $blob_fhandle);
+
+function scrap_folders($w_dir, &$blob_fhandle) {
+	if (!$dir = opendir($w_dir)) {
+		die($w_dir . ' is not a directory' . "\n");
+	}
+	
+	while (false !== ($item = readdir($dir))) {
+		if (!preg_match('/\.[.*]/', $item)) {
+			echo '-- scraping ' . $item . ' ...' . "\n";
+			if (is_dir($item)) {
+				scrap_folders($w_dir . '/' . $item, $blob_fhandle);
+			} else {
+				add_blob_entry($w_dir, $item, $blob_fhandle);
+			}
+		}
+	}
+}
+
+function add_blob_entry($dir, $file, &$blob_fhandle) {
+	$target = str_replace('vendor/samsung/proprietary', '', $dir);
+	fwrite($blob_fhandle, 'PRODUCT_COPY_FILES += vendor/samsung/' . $dir . '/' . $file . ':obj/' . $target . '/' . $entry . "\n");
+	fwrite($blob_fhandle, 'PRODUCT_COPY_FILES += vendor/samsung/' . $dir . '/' . $file . ':system/' . $target . '/' . $entry . "\n");
+}
